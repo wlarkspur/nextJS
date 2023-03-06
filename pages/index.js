f61b6484bb5949e6d850bcdaa1e55a28fc25161d
wlarkspur <newacon@naver.com>
2023-03-06 21:21:23 +0200

    next.config.js redirect/rewrites

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "30a18278aa32c27de99875e2b7b41efe";
-
 export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
